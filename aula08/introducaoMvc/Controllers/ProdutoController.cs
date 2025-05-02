5103a17c18e0f82350af4e15f29f596bd058abc0
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-05-02 15:30:30 -0300

    aula 08 mvc e create

---
+++ aula08/introducaoMvc/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using introducaoMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introducaoMvc.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private static List<Produto> produtos = new List<Produto>();
+        public IActionResult Index() {
+            return View(produtos); // retorna a View "index" com a lista de produtos
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Produto newProduto)
+        {
+            newProduto.Id = produtos.Count + 1;
+            produtos.Add(newProduto);
+            return RedirectToAction("Index");
+        }
+    }
+}
